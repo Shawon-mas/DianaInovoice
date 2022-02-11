729710f35f4860596f6c092fb00e7ce0adf85b6a
User <shawon.mas@gmail.com>
2022-02-11 21:17:21 +0600

    update

--- app/src/main/java/com/dianainovoice/dianainovoice/MainActivity.java
+++ app/src/main/java/com/dianainovoice/dianainovoice/MainActivity.java
@@ -1,26 +1,52 @@
 package com.dianainovoice.dianainovoice;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
-import com.dianainovoice.dianainovoice.Fragment.OneFragment;
-import com.dianainovoice.dianainovoice.Fragment.AccountFragment;
-import com.dianainovoice.dianainovoice.Fragment.HomeFragment;
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 
 
 public class MainActivity extends AppCompatActivity {
  MeowBottomNavigation meowBottomNavigation;
-
+    WebView webView;
+    ProgressBar progressBar_home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView=findViewById(R.id.webview_home);
+        progressBar_home=findViewById(R.id.progressbar_homeAc);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setUseWideViewPort(true);
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient( new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+
+                progressBar_home.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+                progressBar_home.setVisibility(View.GONE);
+            }
+        });
+
+
+        webView.loadUrl("https://dianainvoice.com/");
         meowBottomNavigation=findViewById(R.id.bottom_nav);
         meowBottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.phone));
         meowBottomNavigation.add(new MeowBottomNavigation.Model(2,R.drawable.home));
@@ -28,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
          meowBottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
              @Override
              public void onShowItem(MeowBottomNavigation.Model item) {
-                 Fragment fragment=null;
+
                  switch (item.getId())
                  {
                      case 1:
@@ -36,14 +62,18 @@ public class MainActivity extends AppCompatActivity {
                          Intent intent = new Intent(Intent.ACTION_DIAL);
                          intent.setData(Uri.parse("tel:"+string));
                          startActivity(intent);
+                         overridePendingTransition(0,0);
+                         return;
                      case 2:
-                         fragment=new HomeFragment();
-                         break;
+                         overridePendingTransition(0,0);
+                        return;
                      case 3:
-                         fragment=new AccountFragment();
-                         break;
+                        Intent intent1=new Intent(getApplicationContext(), AccountActivity.class);
+                        startActivity(intent1);
+                         overridePendingTransition(0,0);
+                         return;
                  }
-                     loadFragment(fragment);
+
              }
          });
 
@@ -57,10 +87,23 @@ public class MainActivity extends AppCompatActivity {
       });
     }
 
-    private void loadFragment(Fragment fragment) {
-        getSupportFragmentManager().beginTransaction().
-                replace(R.id.frame_layout,fragment)
-                .commit();
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else
+            {
+            super.onBackPressed();
+        }
 
     }
 }
+/*
+   Fragment fragment=null;
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().
+                replace(R.id.frame_layout,fragment).addToBackStack(null)
+                .commit();
+
+    }
+ */
